8515bfd5e64402fc9060becc2d03b467ff270d77
ShaneDsouza4 <shane.json96@gmail.com>
2024-05-31 20:56:55 -0400

    Configured npm & Installed parcel

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+//Creating a nested div
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement("div",{ id:"child1" },[
+            React.createElement("h1",{},"I'm an h1 tag"),
+            React.createElement("h2",{},"I'm an h2 tag")
+        ]),
+        React.createElement("div", { id:"child2" },[
+            React.createElement("h1",{},"I'm an h1 tag"),
+            React.createElement("h2",{},"I'm an h2 tag")
+        ])
+    ]
+);
+
+console.log(parent);
+
+
+
+
 //Create Element
 const heading = React.createElement(
     "h1",
@@ -6,10 +27,11 @@ const heading = React.createElement(
 );
 
 //Explore the Element
-console.log(heading);
+//console.log(heading);
 
 //Create Root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //Render Heading
-root.render(heading);
+//root.render(heading);
+root.render(parent);
